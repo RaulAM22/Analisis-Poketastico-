7dd8adc0da486abd90076083fcf44b680261c4f7
Megumi-cpu <20241391@lamolina.edu.pe>
2025-12-17 15:16:00 -0500

    inicio

---
+++ TrabajoFinal.py
@@ -0,0 +1,13 @@
+# =========================================
+# PROYECTO LP – ANÁLISIS ESTADÍSTICO CON POKEAPI
+# =========================================
+
+# =========================================
+# 0. IMPORTACIÓN DE LIBRERÍAS
+# =========================================
+
+import requests
+import json
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
