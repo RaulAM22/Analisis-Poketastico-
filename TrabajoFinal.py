77da2b4638e29830d1ad4626d49f16a57cba23ce
Gianella-Cruz <gianellacruzlozano@gmail.com>
2025-12-17 16:04:31 -0500

    Update TrabajoFinal.py
    AGREGANDO LIMPIEZA Y ESTRUCTURA :b

--- TrabajoFinal.py
+++ TrabajoFinal.py
@@ -36,3 +36,60 @@ with open("data/raw_pokemon.json", "w", encoding="utf-8") as f:
     json.dump(pokemon_raw, f, indent=4)
 
 print(f"✔ Se obtuvieron {len(pokemon_raw)} Pokémon\n")
+
+# =========================================
+# 2. LIMPIEZA Y ESTRUCTURACIÓN (GIANE)
+# =========================================
+
+clean_data = []
+
+for p in pokemon_raw:
+    tipos = [t["type"]["name"] for t in p["types"]]
+    tipos_str = " / ".join(tipos)
+
+    imagen_url = p["sprites"]["front_default"]
+
+    clean_data.append({
+        "nombre": p["name"],
+        "tipo": tipos_str,
+        "altura": p["height"],
+        "peso": p["weight"],
+        "experiencia_base": p["base_experience"],
+        "salud": p["stats"][0]["base_stat"],
+        "ataque": p["stats"][1]["base_stat"],
+        "defensa": p["stats"][2]["base_stat"],
+        "imagen": imagen_url
+    })
+
+    if imagen_url:
+        img = requests.get(imagen_url).content
+        with open(f"figures/pokemon_images/{p['name']}.png", "wb") as f:
+            f.write(img)
+
+df = pd.DataFrame(clean_data)
+df.to_csv("data/pokemon_clean.csv", index=False, encoding="utf-8")
+
+# =========================================
+# TRADUCCIÓN DE TIPOS
+# =========================================
+
+traduccion_tipos = {
+    "normal": "Normal",
+    "fire": "Fuego",
+    "water": "Agua",
+    "electric": "Eléctrico",
+    "grass": "Planta",
+    "ice": "Hielo",
+    "fighting": "Lucha",
+    "poison": "Veneno",
+    "ground": "Tierra",
+    "flying": "Volador",
+    "psychic": "Psíquico",
+    "bug": "Bicho",
+    "rock": "Roca",
+    "ghost": "Fantasma",
+    "dark": "Siniestro",
+    "dragon": "Dragón",
+    "steel": "Acero",
+    "fairy": "Hada"
+}
